d00fffdc08b2ceb99f56b5ed77933bd488546820
dimavagon <dimavagon8@gmail.com>
2025-06-17 15:23:09 +0500

    My suka first project

---
+++ project.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+int main(void){
+    printf("My suka first project\n");
+
+    return 0;
+}
